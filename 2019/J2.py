1698397d6f1eed26d048efa99f62576169eacd95
dAIsySHEng1 <45747761+dAIsySHEng1@users.noreply.github.com>
2019-08-16 12:09:41 -0400

    Update J2.py

--- 2019/J2.py
+++ 2019/J2.py
@@ -1,1 +1,19 @@
+def message_return():
+    l = input()
+    L = int(l)
+    i = 0
+    j = []
+    while i < L:
+        s = input()
+        a = s.split()
+        for x in a:
+            m = x
+            j.append(m)
+        i = i + 1
 
+    line = ''
+    for i in range(0, len(j), 2):
+        line = int(j[i])*j[i+1]
+        print(line)
+
+message_return()
