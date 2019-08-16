5c873a9831965a582c852da0f67a6929f177a9fd
dAIsySHEng1 <45747761+dAIsySHEng1@users.noreply.github.com>
2019-08-16 12:16:07 -0400

    Update J2.py

--- 2018/J2.py
+++ 2018/J2.py
@@ -1,1 +1,10 @@
 
+spaces = input()
+yesterday = input()
+today = input()
+
+same = 0
+for i in range(0, int(spaces)):
+    if yesterday[i] == today[i] and today[i]=='C':
+        same = same + 1
+print(same)
