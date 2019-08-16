63bc357d54354c9b39aa446388204b5e758640a7
dAIsySHEng1 <45747761+dAIsySHEng1@users.noreply.github.com>
2019-08-16 12:15:17 -0400

    Update J1.py

--- 2018/J1.py
+++ 2018/J1.py
@@ -1,1 +1,11 @@
 
+fourth = input()
+third = input()
+second = input()
+last = input()
+
+if (fourth=='8' or fourth =='9') and third==second and (last=='8' or last=='9'):
+  print('ignore')
+else:
+  print('answer')
+  
