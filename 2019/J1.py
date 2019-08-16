efd5b52f9d10fe9a438fefe96c3bd732705fe124
dAIsySHEng1 <45747761+dAIsySHEng1@users.noreply.github.com>
2019-08-16 12:10:37 -0400

    Update J1.py

--- 2019/J1.py
+++ 2019/J1.py
@@ -1,1 +1,25 @@
 
+def basket_counter():
+    N = 6
+    i = 0
+    j = []
+    while i < N:
+        s = input()
+        a = s.split()
+        for x in a:
+            m = int(x)
+            j.append(m)
+        i = i + 1
+
+    m = 0
+    score_apples = int(j[m])*3 + int(j[m+1])*2 + int(j[m+2])*1
+    score_bananas = int(j[m+3])*3 + int(j[m+4])*2+int(j[m+5])*1
+
+    if score_apples == score_bananas:
+        print('T')
+    elif score_apples > score_bananas:
+        print ('A')
+    else:
+        print('B')
+
+basket_counter()
