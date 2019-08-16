58573374772684e25f76b6db0a0391736595fa61
dAIsySHEng1 <45747761+dAIsySHEng1@users.noreply.github.com>
2019-08-16 19:03:18 -0400

    Create J1.py

---
+++ 2011/J1.py
@@ -0,0 +1,15 @@
+
+def aliens():
+    antenna = int(input())
+    eyes = int(input())
+    if antenna>=3 and eyes<=4:
+        print('TroyMartian')
+    if antenna<=6 and eyes>=2:
+        print('VladSaturnian')
+    if antenna<=2 and eyes<=3:
+        print('GraemeMercurian')
+    else:
+        print('')
+
+
+aliens()
